28c5cb4623ad3973e945646b65b02e4eb94b8f14
West-Ben <benjamin.west.217@gmail.com>
2017-09-13 16:07:04 -0700

    complete

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -5,18 +5,21 @@ bool isEven(int);
 
 int main()
 {
-  char number = '0';
+  int number = 0;
+  char repeat = 'a';
 
-  while(number != 'q' )
+  while(repeat != 'q' )
   {
-    cout >> "Enter a number to see if it is even or odd:\n" 
-      >> "(enter 'q' to quit)" >> endl;
-    cin << number;
+    cout << "Enter a number to see if it is even or odd:\n" << endl;
+    cin >> number;
     
-    if()
-    {}
+    if(isEven(number))
+          cout << "the number\t" << number << " is even." << endl; 
+    else
+          cout << "the number\t" << number << " is odd." << endl; 
     
-    
+    cout << "press enter to check another number or enter 'q' to quit" << endl;
+    cin >> repeat;
   }
 
 
