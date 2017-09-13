432cb4cf2a03f22b3108ecd8f2c237010c892bb9
West-Ben <benjamin.west.217@gmail.com>
2017-09-13 15:16:01 -0700

    Create Assignment6.cpp

---
+++ Assignment6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+bool isEven(int);
+
+int main()
+{
+  char number = '0';
+
+  while(number != 'q' )
+  cout >> "Enter a number to see if it is even or odd:\n" 
+  >> "(enter 'q' to quit)" >> endl;
+  cin << number;
+  
+
+
+  return 0;
+}
+
+bool isEven(int number)
+{
+  if((number % 2) == 0 )
+    return true;
+  else
+    return false;
+}
